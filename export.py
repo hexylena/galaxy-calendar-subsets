277d17436b147a0dfe1208f4246f06ea4032eabe
Helena Rasche <hexylena@galaxians.org>
2024-02-06 14:55:04 +0100

    try wrapping in cal

--- export.py
+++ export.py
@@ -17,8 +17,19 @@ for event in calendar.walk('VEVENT'):
             if re.match(m, str(event['SUMMARY']), re.IGNORECASE):
                 OUTPUTS[cal].append(event)
 
-
 for k, events in OUTPUTS.items():
+    header = f"""BEGIN:VCALENDAR
+PRODID:-//Github Calendar Subset Exporter//hexylena 1.0//EN
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+X-WR-CALNAME:Galaxy {k} Calendar
+X-WR-TIMEZONE:UTC
+X-WR-CALDESC:Subset of the official Galaxy Working Groups Calendar
+"""
+
     with open(f"{k}.ics", 'w') as handle:
+        handle.write(header)
         for event in events:
             handle.write(event.to_ical().decode('utf-8'))
+        handle.write("END:VCALENDAR")
